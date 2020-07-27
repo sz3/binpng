37a6962417cf8ecc192f32c8ec66f5296c7c93f4
sz <sz@galacticicecube.com>
2020-07-27 10:49:21 -0500

    An idea

---
+++ binpng.py
@@ -0,0 +1,46 @@
+import sys
+from math import sqrt
+
+from PIL import Image
+
+
+def _get_best_dimensions(n):
+    small_divisors = [d for d in range(2, int(sqrt(n))) if n % d == 0]
+    if not small_divisors:
+        return (n, 1)
+
+    # divisors[-1] is the largest divisor below sqrt(n)
+    # we'll use it as the height, and the bigger number as the width
+    height = small_divisors[-1]
+    return (n // height, height)
+
+
+def decode(src_file, dst_file):
+    img = Image.open(src_file)
+    with open(dst_file, 'wb') as f:
+        for p in img.getdata():
+            bites = bytearray(p)
+            f.write(bites)
+
+
+def encode(src_file, dst_file):
+    with open(src_file, 'rb') as f:
+        contents = f.read()
+
+    pixels = len(contents) // 3
+    imgSize = _get_best_dimensions(pixels)
+    img = Image.frombytes('RGB', imgSize, contents, 'raw')
+    img.save(dst_file)
+
+
+def main():
+    src_file = sys.argv[1]
+    dst_file = sys.argv[2]
+    if src_file.endswith('.png'):
+        decode(src_file, dst_file)
+    else:
+        encode(src_file, dst_file)
+
+
+if __name__ == '__main__':
+    main()
